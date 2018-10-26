654a7110d4c443a1341e295645bf56ec57ea34e5
System Administrator <root@keyrwinf.local>
2018-10-26 18:39:31 +0800

    initial burger builder with controls

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
+import Aux from '../../hoc/Aux';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
+		},
+		totalPrice: 4
+	}
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + INGREDIENT_PRICES[type];
+		this.setState({
+			ingredients: updatedIngredients,
+			totalPrice: newPrice
+		})
+	}
+
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if(oldCount <= 0){
+			return;
+		}
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - INGREDIENT_PRICES[type];
+		this.setState({
+			ingredients: updatedIngredients,
+			totalPrice: newPrice
+		})
+	}
+
+	render () {
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+		for(let key in disabledInfo){
+			disabledInfo[key] = disabledInfo[key] <= 0
+		}
+
+		return (
+			<Aux>
+				<Burger ingredients={this.state.ingredients} />
+				<BuildControls
+					price={this.state.totalPrice}
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+				/>
+			</Aux>
+		);
+	}
+}
+export default BurgerBuilder;
